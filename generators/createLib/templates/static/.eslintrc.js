56f6995029a41dcd9b809c225e87b51b0b72debe
Roman Mashta <roman.mashta@gmail.com>
2020-06-10 02:17:13 +0300

    Library template updates

--- generators/createLib/templates/static/.eslintrc.js
+++ generators/createLib/templates/static/.eslintrc.js
@@ -1,11 +1,14 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    project: "./tsconfig.eslint.json",
+    project: './tsconfig.eslint.json'
   },
-  extends: ["airbnb-typescript-prettier"],
-  "env": {
-    "jest": true
+  extends: ['airbnb-typescript-prettier'],
+  env: {
+    jest: true
   },
-  ignorePatterns: ["dist", "*.config.js", ".*.js"],
+  ignorePatterns: ['dist', '*.config.js', '.*.js'],
+  rules: {
+    'import/prefer-default-export': 'off'
+  }
 };
