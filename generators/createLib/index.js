d5b70a4d86e05edc5d7adf58d7717e0a6f401f88
Roman Mashta <roman.mashta@gmail.com>
2020-06-13 02:54:53 +0300

    Disable auto yarn install

--- generators/createLib/index.js
+++ generators/createLib/index.js
@@ -1,38 +1,40 @@
-'use strict';
-const Generator = require('yeoman-generator');
-const chalk = require('chalk');
-const yosay = require('yosay');
-const path = require('path');
-const initRepoQuestions = require('../initRepo/questions');
-const {fromQuestions, repoProps} = require('../../utils');
+"use strict";
+const Generator = require("yeoman-generator");
+const chalk = require("chalk");
+const yosay = require("yosay");
+const path = require("path");
+const initRepoQuestions = require("../initRepo/questions");
+const { fromQuestions, repoProps } = require("../../utils");
 
-const LIB_ROOT = 'lib';
+const LIB_ROOT = "lib";
 
 module.exports = class extends Generator {
-  preRun(){
+  preRun() {
     this.props._rootDir = this.props._rootDir || this.env.cwd;
-    console.log('root-path', this.props._rootDir);
+    console.log("root-path", this.props._rootDir);
   }
 
   prompting() {
     this.log(
-      yosay(`Welcome to the perfect ${chalk.red('generator-monospace')} generator!`)
+      yosay(
+        `Welcome to the perfect ${chalk.red("generator-monospace")} generator!`
+      )
     );
 
     const prompt = {
       appName: {
         type: "input",
-        message: "Specify library name",
+        message: "Specify library name"
       }
     };
 
-    const {scopeName, gitUser} = initRepoQuestions;
-    const prompts = fromQuestions(this, {...prompt, scopeName, gitUser});
+    const { scopeName, gitUser } = initRepoQuestions;
+    const prompts = fromQuestions(this, { ...prompt, scopeName, gitUser });
 
     return this.prompt(prompts).then(props => {
       const derived = {
         ...props,
-        packageName: `${LIB_ROOT}.${props.appName}`,
+        packageName: `${LIB_ROOT}.${props.appName}`
       };
       this.props = {
         ...derived,
@@ -42,38 +44,32 @@ module.exports = class extends Generator {
   }
 
   default() {
-    this.composeWith(require.resolve('../initRepo'), {
+    this.composeWith(require.resolve("../initRepo"), {
       ...this.props,
-      skipRoot: true,
+      skipRoot: true
     });
   }
 
   app() {
-    const {repo, _rootDir} = this.props;
+    const { repo, _rootDir } = this.props;
     this.destinationRoot(path.join(_rootDir, repo.packageRoot));
   }
 
   writing() {
-    const {repo} = this.props;
-    this.fs.copy(
-      this.templatePath('static/**/*'),
-      this.destinationRoot()
-    );
-    this.fs.copy(
-      this.templatePath('static/**/.*'),
-      this.destinationRoot()
-    );
+    const { repo } = this.props;
+    this.fs.copy(this.templatePath("static/**/*"), this.destinationRoot());
+    this.fs.copy(this.templatePath("static/**/.*"), this.destinationRoot());
     this.fs.copyTpl(
-      this.templatePath('package.json'),
-      this.destinationPath('package.json'),
+      this.templatePath("package.json"),
+      this.destinationPath("package.json"),
       { repo: repo }
     );
   }
 
   async install() {
-    const {_rootDir} = this.props;
-    const rootScope = {cwd: _rootDir}
+    const { _rootDir } = this.props;
+    const rootScope = { cwd: _rootDir };
 
-    this.spawnCommandSync('yarn', [], rootScope);
+    //this.spawnCommandSync("yarn", [], rootScope);
   }
 };
